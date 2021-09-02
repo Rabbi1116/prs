a2b9c4af624446bd26aa5ad39fb882196c94dfbf
Rabbi1116 <rabbihossain1116@gmail.com>
2021-09-02 01:30:20 -0700

    before show data

--- resources/views/admin/include/menu.blade.php
+++ resources/views/admin/include/menu.blade.php
@@ -4,7 +4,7 @@
     <a href="" class="brand-link ">
         <img src="{{asset('admin/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-4" style="opacity: .8">
-        <span class="brand-text text-light" >IT WAY BD</span>
+        <span class="brand-text text-light">IT WAY BD</span>
     </a>
 
     <!-- Sidebar -->
@@ -15,6 +15,7 @@
                 <img src="{{asset('admin/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-4 "
                     alt="User Image">
             </div>
+            
             <div class="info">
                 <a href="#" class="d-block"></a>
             </div>
@@ -32,11 +33,12 @@
                     <div class="input-group" data-widget="sidebar-search">
                         <input class="form-control form-control-sidebar" type="search" placeholder="Search"
                             aria-label="Search">
-                       
+
                     </div>
                 </div>
                 <li class="nav-item menu-open">
-                    <a href="{{route('home')}}"   class=" {{ (request()->is('home')) ? 'active' : '' }} nav-link list-group-item-action btn btn-outline-primary">
+                    <a href="{{route('home')}}"
+                        class=" {{ (request()->is('home')) ? 'active' : '' }} nav-link list-group-item-action btn btn-outline-primary">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Dashboard
@@ -45,7 +47,8 @@
                 </li>
 
                 <li class="nav-item menu-open">
-                    <a href="{{route('clientlist')}}" class="{{ (request()->is('Client-list')) ? 'active' : '' }} nav-link  list-group-item-action btn btn-outline-primary">
+                    <a href="{{route('clientlist')}}"
+                        class="{{ (request()->is('Client-list')) ? 'active' : '' }} nav-link  list-group-item-action btn btn-outline-primary">
                         <i class="fas fa-building"></i>
                         <p>
                             Client List
@@ -54,7 +57,8 @@
                 </li>
 
                 <li class="nav-item menu-open">
-                    <a href="{{route('status')}}" class="{{ (request()->is('Status')) ? 'active' : '' }}  nav-link  list-group-item-action btn btn-outline-primary">
+                    <a href="{{route('status')}}"
+                        class="{{ (request()->is('Status')) ? 'active' : '' }}  nav-link  list-group-item-action btn btn-outline-primary">
                         <i class="fas fa-clipboard-list"></i>
                         <p>
                             Status
@@ -63,7 +67,8 @@
                 </li>
 
                 <li class="nav-item menu-open">
-                    <a href="{{route('Setup')}}" class="{{ (request()->is('Setup')) ? 'active' : '' }} nav-link  list-group-item-action btn btn-outline-primary">
+                    <a href="{{route('Setup')}}"
+                        class="{{ (request()->is('Setup')) ? 'active' : '' }} nav-link  list-group-item-action btn btn-outline-primary">
                         <i class="fas fa-cog"></i>
                         <p>
                             Setup
@@ -72,24 +77,27 @@
                 </li>
 
                 <li class="nav-item menu-open">
-                    <a href="{{route('area_page_view')}}" class="{{ (request()->is('Area-setup')) ? 'active' : '' }}  nav-link  list-group-item-action btn btn-outline-primary">
-                    <i class="fas fa-map-marker-alt"></i>
+                    <a href="{{route('area_page_view')}}"
+                        class="{{ (request()->is('Area-setup')) ? 'active' : '' }}  nav-link  list-group-item-action btn btn-outline-primary">
+                        <i class="fas fa-map-marker-alt"></i>
                         <p>
-                          Area Setup
+                            Area Setup
                         </p>
                     </a>
                 </li>
                 <li class="nav-item menu-open">
-                    <a href="{{route('visit_list')}}" class="{{ (request()->is('Visit-list')) ? 'active' : '' }} nav-link  list-group-item-action btn btn-outline-primary">
-                    <i class="fas fa-eye"></i>
+                    <a href="{{route('visit_list')}}"
+                        class="{{ (request()->is('Visit-list')) ? 'active' : '' }} nav-link  list-group-item-action btn btn-outline-primary">
+                        <i class="fas fa-eye"></i>
                         <p>
-                         visit
+                            visit
                         </p>
                     </a>
                 </li>
 
                 <li class="nav-item menu-open">
-                    <a href="{{route('user_page_view')}}" class="{{ (request()->is('User')) ? 'active' : '' }} nav-link  list-group-item-action btn btn-outline-primary">
+                    <a href="{{route('user_page_view')}}"
+                        class="{{ (request()->is('User')) ? 'active' : '' }} nav-link  list-group-item-action btn btn-outline-primary">
                         <i class="fas fa-users"></i>
                         <p>
                             Users
@@ -98,7 +106,8 @@
                 </li>
 
                 <li class="nav-item menu-open">
-                    <a href="{{route('project')}}" class="{{ (request()->is('Project')) ? 'active' : '' }} nav-link  list-group-item-action btn btn-outline-primary">
+                    <a href="{{route('project')}}"
+                        class="{{ (request()->is('Project')) ? 'active' : '' }} nav-link  list-group-item-action btn btn-outline-primary">
                         <i class="fas fa-tasks"></i>
                         <p>
                             Project
@@ -107,7 +116,8 @@
                 </li>
 
                 <li class="nav-item menu-open">
-                    <a href="/viewregister" class="{{ (request()->is('viewregister')) ? 'active' : '' }} nav-link  list-group-item-action btn btn-outline-primary">
+                    <a href="/viewregister"
+                        class="{{ (request()->is('viewregister')) ? 'active' : '' }} nav-link  list-group-item-action btn btn-outline-primary">
                         <i class="fas fa-user-cog"></i>
                         <p>
                             Add New Employee
